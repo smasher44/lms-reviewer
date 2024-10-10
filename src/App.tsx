6021d7423e19d5fdf0b84a411b9bfe9e3d20d620
smasher44 <georgelibatique819@gmail.com>
2024-10-10 23:37:30 +0800

    [SETUP] first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { Button } from "@/components/ui/button"
+
+
+function App() {
+ 
+  return (
+    <div className="flex flex-col gap-4 justify-center items-center h-screen"> 
+      <Button variant="default">Default</Button>
+      <Button variant="secondary">Secondary</Button>
+      <Button variant="destructive">Destructive</Button>
+      <Button variant="outline">Outline</Button>
+      <Button variant="link">Link</Button>
+    </div>
+  )
+}
+
+export default App
